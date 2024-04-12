52abfb82fc1db887d8df816be6f8450154b7f3fc
Heiazel <100291065+Heiazel@users.noreply.github.com>
2024-04-12 12:26:07 +0200

    Update hist.c

--- Examen/hist.c
+++ Examen/hist.c
@@ -2,32 +2,14 @@
 #include<stdlib.h>
 
 void display_histogram(int tab[], int size) {
-    
-    int max = tab[0];
-    for (int i = 1; i < size; i++) {
-        if (tab[i] > max) {
-            max = tab[i];
-        }
-    }
-
-    
-    for (int i = 0; i <= max; i++) {
-        
-        int occurrences = 0;
-        for (int j = 0; j < size; j++) {
-            if (tab[j] == i) {
-                occurrences++;
-            }
-        }
-        
-        
-        printf("%2d |", i);
-        for (int k = 0; k < occurrences; k++) {
+    int i,j;
+     for (i = 0; i < size; i++) {
+        for (j = 0; j < tab[i]; j++) {
             printf("*");
         }
         printf("\n");
+        }
     }
-}
 
 int main() {
     int values[] = { 4, 9, 8, 2, 0, 1, 10, 5, 8 };
